61927f17ee549ae26c9b54fcce63d1a497632ff8
Velislav <v.peychev@yahoo.com>
2017-05-22 12:00:10 +0200

    index and login

--- index.php
+++ index.php
@@ -8,7 +8,14 @@
     <link rel="stylesheet" type="text/css" href="style/style.css">
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+    <?php
+    if(isset($_GET['page'])) {
+        $view = $_GET['page'];
+    }else {
+        $view = "login";
+    }
+    include_once "views/$view.php";
+    ?>
 </head>
 <body>
 
---
+++ views/login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Velislav
+ * Date: 22/05/2017
+ * Time: 11:55
+ */
+
+$content =
+    "<div class=\"wdw-login\">
+    <form method=\"get\">
+        <input type=\"text\" id=\"lbl-username\" placeholder=\"username\">
+        <input type=\"text\" id=\"lbl-password\" placeholder=\"password\">
+        <input type=\"submit\" id=\"btn-login-submit\" value=\"Log in\">
+    </form>";
