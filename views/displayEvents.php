2bd792124a8b9f1ae7dc674115cddc4f6e0fa538
Velislav <v.peychev@yahoo.com>
2017-05-22 17:16:48 +0200

    displayEvents

--- views/displayEvents.php
+++ views/displayEvents.php
@@ -6,4 +6,31 @@
  * Time: 15:39
  */
 
-$content = "These are all the events...";
+$asEvents = file_get_contents("events.txt");
+$ajEvents = json_decode($asEvents);
+
+$allEvents="";
+
+foreach($ajEvents as $jEvent)
+{
+    $allEvents.="<div class='event'>
+        <div class='wdw-event-img'>
+            <img src='$jEvent->image' alt='event image'>
+        </div>
+        <div class='wdw-event-info'>
+            <div>
+                <h3>$jEvent->eventname</h3>
+                <h5>Partner: $jEvent->partner</h5>
+            </div>
+            <div>
+                <h5>Theme: $jEvent->theme</h5>
+                <p>$jEvent->description</p>
+            </div>
+            <div>
+                <h5>$jEvent->date</h5>
+                <h5>$jEvent->capacity</h5>
+            </div>
+        </div>
+    </div>";
+}
+$content = $allEvents;
