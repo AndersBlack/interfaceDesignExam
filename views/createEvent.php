bea99aa3adcb300fcc60ff38dbf20d2daebe5766
Michael <gnimee@gmail.com>
2017-05-22 18:45:27 +0200

    CreateEvent mm

--- views/createEvent.php
+++ views/createEvent.php
@@ -1,9 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Velislav
- * Date: 22/05/2017
- * Time: 15:42
- */
 
-$content = "This is the create events page";
+
+$content = "<div class=\"wdw-createEvent\">
+    <form>
+
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Name of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-nameEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Location of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-localEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Theme of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-themeEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Date of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-dateEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Description of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-descriptionEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Partner of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-partnerEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label> Capacity of the Event:</label>
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-capacityEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Manager of the Event:</label>
+            <select id=\"drp-users\">
+            <!--<input type=\"text\" class=\"createEvent form-control\">-->
+            </select>
+        </div>
+
+        <div class=\"wdw-createEvent-InputArea\">
+            <label>Picture of the Event:</label>
+            <input type=\"file\" class=\"createEvent form-control\" id=\"lbl-pictureEvent\">
+        </div>
+        <div class=\"wdw-createEvent-InputArea\">
+            <input type=\"button\" class=\"btn btn-primary\" id=\"btn-submitEvent\" value=\"Create event\">
+        </div>
+
+    </form>
+
+</div>";
+
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+//<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+//<script>
+//$( function() {
+//    $( "#lbl-dateEvent" ).datepicker();
+//} );
+//
+//
+//    $("#btn-submitEvent").click(function () {
+//
+//
+//        var fieldName = $("#lbl-nameEvent").val();
+//        var fieldLocal = $("#lbl-localEventEvent").val();
+//        var fieldTheme = $("#lbl-themeEventEvent").val();
+//        var fieldDate = $("#lbl-dateEventEvent").val();
+//        var fieldEvent = $("#lbl-Event").val();
+//        var fielDescription = $("#lbl-descriptionEvent").val();
+//        var fieldPartner = $("#lbl-partnerEvent").val();
+//        var fieldCapacity = $("#lbl-capacityEvent").val();
+//        var fieldManager = $("#drp-users").val();
+//        var fieldPicture = $("#lbl-pictureEvent").val();
+//
+//
+//    });
+//</script>
