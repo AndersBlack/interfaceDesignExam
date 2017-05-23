d0ad095a01b60e00d18b34b750be8843f2caf91d
Velislav <v.peychev@yahoo.com>
2017-05-23 13:14:33 +0200

    select dropdown

--- views/createEvent.php
+++ views/createEvent.php
@@ -1,19 +1,7 @@
 <?php
 
-$users = file_get_contents("../users.txt");
-$jData = json_decode($users);
-$options = [];
-
-foreach ( $jData as $jObject) {
-    $options[$jObject['username']];
-    //echo '<jObject username=' . $jObject->id . '>'. $jObject->username . '</jObject>';
-   // echo $jObject->username performer;
-    
-};
-
-
-
-
+$users = file_get_contents("users.txt");
+$ajUsers = json_decode($users);
 
 $content = "<div class=\"wdw-createEvent\">
     <form>
@@ -48,14 +36,20 @@ $content = "<div class=\"wdw-createEvent\">
         </div>
         <div class=\"wdw-createEvent-InputArea\">
             <label>Manager of the Event:</label>
-            <select id=\"drp-users\">
-              <!--<input type=\"text\" class=\"createEvent form-control\">-->
+            <select id=\"drp-users\">";
+
+foreach ( $ajUsers as $jUser) {
+    $content .= "<option value='$jUser->username'>$jUser->username</option>";
+};
+
+
+$content .="<!--<input type=\"text\" class=\"createEvent form-control\">-->
             </select>
         </div>
 
         <div class=\"wdw-createEvent-InputArea\">
             <label>Picture of the Event:</label>
-            <input type=\"file\" class=\"createEvent form-control\" id=\"lbl-pictureEvent\">
+            <input type=\"text\" class=\"createEvent form-control\" id=\"lbl-pictureEvent\">
         </div>
         <div class=\"wdw-createEvent-InputArea\">
             <input type=\"button\" class=\"btn btn-primary\" id=\"btn-submitEvent\" value=\"Create event\">
@@ -64,28 +58,3 @@ $content = "<div class=\"wdw-createEvent\">
     </form>
 
 </div>";
-    //<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-//<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-//<script>
-//$( function() {
-//    $( "#lbl-dateEvent" ).datepicker();
-//} );
-//
-//
-//    $("#btn-submitEvent").click(function () {
-//
-//
-//        var fieldName = $("#lbl-nameEvent").val();
-//        var fieldLocal = $("#lbl-localEventEvent").val();
-//        var fieldTheme = $("#lbl-themeEventEvent").val();
-//        var fieldDate = $("#lbl-dateEventEvent").val();
-//        var fieldEvent = $("#lbl-Event").val();
-//        var fielDescription = $("#lbl-descriptionEvent").val();
-//        var fieldPartner = $("#lbl-partnerEvent").val();
-//        var fieldCapacity = $("#lbl-capacityEvent").val();
-//        var fieldManager = $("#drp-users").val();
-//        var fieldPicture = $("#lbl-pictureEvent").val();
-//
-//
-//    });
-//</script>
